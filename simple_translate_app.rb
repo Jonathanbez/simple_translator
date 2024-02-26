753290614050e5b9b3d5f5a673a08e2e7bdda46d
Jonathanbez <jbbarroz@gmail.com>
2024-02-26 07:53:11 -0300

    Project Final OneBitCode, make a app

--- simple_translate_app.rb
+++ simple_translate_app.rb
@@ -32,7 +32,7 @@ puts translater_output.translater(src_sentence, src_l, trg_l)
 class Saver_text
   def save_log(src_sentence, translater_output)
     time = Time.now
-    name = time.strftime('%d/%m/%Y %H:%m:%S')
+    name = time.strftime('%d-%m-%Y %H.%M.%S')
     File.open("#{name}.txt", 'w') do |saver|
       saver.puts "Input text: #{src_sentence}"
       saver.puts "Output text: #{translater_output}"
