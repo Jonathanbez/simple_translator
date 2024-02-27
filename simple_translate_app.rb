ec87747f00910d14f424a87a038921edbd8a1708
Jonathanbez <jbbarroz@gmail.com>
2024-02-27 07:58:23 -0300

    Project Final OneBitCode, make a app

--- simple_translate_app.rb
+++ simple_translate_app.rb
@@ -2,12 +2,15 @@
 require_relative 'translator'
 require_relative 'log'
 
+puts "SIMPLE TRANSLATER APP\n\n"
+
 prompt = TTY::Prompt.new
 languages = %w[en pt-BR es fr]
 src_l = prompt.select('Select your source language:', languages)
 puts "\n"
 trg_l = prompt.select('select the targed language:', languages)
 
+puts "Write your sentence:"
 src_sentence = gets.chomp
 
 translater_output = Translator.new
