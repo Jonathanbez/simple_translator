0bce12ee31adaa96f5c797b81941ad4e991f19c6
Jonathanbez <jbbarroz@gmail.com>
2024-02-27 07:39:26 -0300

    Project Final OneBitCode, make a app

--- log.rb
+++ log.rb
@@ -1,12 +1,12 @@
 require 'tty-prompt'
 
 class Saver_text
-  def save_log(src_sentence, translater_output)
+  def save_log(src_sentence, output)
     time = Time.now
     name = time.strftime('%d-%m-%Y %H:%M:%S')
     File.open("#{name}.txt", 'w') do |saver|
       saver.puts "Input text: #{src_sentence}"
-      saver.puts "Output text: #{translater_output}"
+      saver.puts "Output text: #{output}"
     end
   end
 end
