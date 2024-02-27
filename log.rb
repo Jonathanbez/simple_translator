15909b13b7c7b2c851b464a424546a3eec831876
Jonathanbez <jbbarroz@gmail.com>
2024-02-27 06:24:07 -0300

    Project Final OneBitCode, make a app

--- log.rb
+++ log.rb
@@ -3,7 +3,7 @@ require 'tty-prompt'
 class Saver_text
   def save_log(src_sentence, translater_output)
     time = Time.now
-    name = time.strftime('%d-%m-%Y %H.%M.%S')
+    name = time.strftime('%d-%m-%Y %H:%M:%S')
     File.open("#{name}.txt", 'w') do |saver|
       saver.puts "Input text: #{src_sentence}"
       saver.puts "Output text: #{translater_output}"
--- translator.rb
+++ translator.rb
@@ -6,15 +6,16 @@ require 'tty-prompt'
 class Translator
   def translater(src_sentence, src_l, trg_l)
     uri = URI("https://api.mymemory.translated.net/get?q=#{src_sentence}&langpair=#{src_l}|#{trg_l}")
-    response = Net::HTTP.get_response(uri)
+    response = Net::HTTP.get(uri)
 
     if response.is_a?(Net::HTTPSuccess)
-      parsed_response = JSON.parse(response.body)
-      parsed_response['responseData']['translatedText']
-
+      call = JSON.parse(response.body)
+      parsed_response = call['responseData']['translatedText']
+      return parsed_response
     else
       response.is_a(Net::HTTPError)
-      JSON.parse(response.body)
+      error = JSON.parse(response.body)
+      return error
     end
   end
 end
