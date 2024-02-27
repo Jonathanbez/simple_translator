c5c5e917a660a177202a17a2333bb0aa7737fedc
Jonathanbez <jbbarroz@gmail.com>
2024-02-27 07:01:58 -0300

    Project Final OneBitCode, make a app

--- translator.rb
+++ translator.rb
@@ -6,18 +6,16 @@ require 'tty-prompt'
 class Translator
   def translater(src_sentence, src_l, trg_l)
     uri = URI("https://api.mymemory.translated.net/get?q=#{src_sentence}&langpair=#{src_l}|#{trg_l}")
-    response = Net::HTTP.get(uri)
+    response = Net::HTTP.get_response(uri)
 
     if response.is_a?(Net::HTTPSuccess)
       call = JSON.parse(response.body)
       parsed_response = call['responseData']['translatedText']
       return parsed_response
     else
-      #response.is_a?(Net::HTTPError)
-      #error = JSON.parse(response.body)
-      return call['error']['info']
-      #JSON::ParserError
-      #return error
+      #response.is_a(Net::HTTPError)
+      #JSON.parse(response.body)
+      #raise "API request failed with status code: #{response.code}"
     end
   end
 end
