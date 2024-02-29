bea4a29526fea7f14dd07635cd5afb93be380d9a
Jonathanbez <jbbarroz@gmail.com>
2024-02-29 07:44:16 -0300

    Project Final OneBitCode, make a app

--- simple_translate_app.rb
+++ simple_translate_app.rb
@@ -2,35 +2,28 @@
 require_relative 'translator'
 require_relative 'log'
 
-
-
-
-
-select_lang = TTY::Prompt.new
-languages = %w[en pt-BR es fr]
-src_l = select_lang.select('Select your source language:', languages)
-puts "\n"
-trg_l = select_lang.select('select the targed language:', languages)
-
-
-
 loop do
   puts "SIMPLE TRANSLATER APP\n\n"
 
   puts "Write your sentence:"
   src_sentence = gets.chomp
 
+  select_lang = TTY::Prompt.new
+  languages = %w[en pt-BR es fr]
+  src_l = select_lang.select('Select your source language:', languages)
+  puts "\n"
+  trg_l = select_lang.select('select the targed language:', languages)
+
   translater_output = Translator.new
   output = translater_output.translater(src_sentence, src_l, trg_l)
   loop_prompt = TTY::Prompt.new
-  select = %w('Yes', 'No')
-  loop_prompt.mult_select("Do you want to try again?", select)
-  break if
-    select == 'No'
+  select = %w(Yes No)
+  select_loop = loop_prompt.select("Do you want to try again?", select)
+  break if select_loop == 'No'
 end
 
 
 
 puts output
 s_file = Saver_text.new
-s_file.save_log(src_sentence, output) 
+s_file.save_log(src_sentence, output)
