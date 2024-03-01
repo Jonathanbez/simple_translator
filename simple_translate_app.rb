a2a44ed2c2fb1c56600a9666df05968f57970976
Jonathanbez <jbbarroz@gmail.com>
2024-03-01 08:13:27 -0300

    Project Final OneBitCode, make a app

--- simple_translate_app.rb
+++ simple_translate_app.rb
@@ -1,29 +1,35 @@
 
 require_relative 'translator'
 require_relative 'log'
+require 'tty-prompt'
+require 'tty-box'
 
 loop do
-    puts "SIMPLE TRANSLATER APP\n\n"
+  system 'clear'
+    puts "SIMPLE TRANSLATER APP(Supports UTF-8 encoding and a maximum of 500 bytes)\n\n"
 
     select_lang = TTY::Prompt.new
-    languages = %w[en pt-BR es fr]
-    src_l = select_lang.select('Select your source language:', languages)
-    puts "\n"
-    trg_l = select_lang.select('select the targed language:', languages)
 
-    puts "Write your sentence:"
+    languages = %w[en pt-BR es fr]
+    src_l = select_lang.select('SELECT YOUR SOURCE LANGUAGE:\n', languages)
+    trg_l = select_lang.select('SELECT THE TARGED LANGUAGE:\n', languages)
+    if src_l == trg_l
+      puts "PLEASE SELECT TWO DISTINCT LANGUAGES"
+      next
+    end
+
+    puts "WRITE YOUR SENTENCE:\n"
     src_sentence = gets.chomp
     puts "\n"
 
     translater_output = Translator.new
     output = translater_output.translater(src_sentence, src_l, trg_l)
-    puts "Your translater:\n#{output}\n\n"
+    puts "YOUR TRANSLATER:\n#{output}\n\n"
     s_file = Saver_text.new
     s_file.save_log(src_sentence, output)
 
     loop_prompt = TTY::Prompt.new
-      select = ['Yes', 'No']
-    select_loop = loop_prompt.select("Do you want to try again?", select)
-  break if
-    select_loop == 'No'
+      select = ['YES', 'NO']
+    select_loop = loop_prompt.select("DO YOU WANT TO TRY AGAIN", select)
+  break if select_loop == 'NO'
 end
--- translator.rb
+++ translator.rb
@@ -1,6 +1,6 @@
 require 'net/http'
 require 'json'
-require 'tty-prompt'
+
 
 
 class Translator
