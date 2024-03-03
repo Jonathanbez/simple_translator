fbefc62f2a5569e5a581d10ede6cf00b46215edb
Jonathanbez <jbbarroz@gmail.com>
2024-03-03 18:45:02 -0300

    Project Final OneBitCode

--- simple_translate_app.rb
+++ simple_translate_app.rb
@@ -3,45 +3,46 @@ require_relative 'translator'
 require_relative 'log'
 require 'tty-prompt'
 require 'tty-spinner'
+require 'colorize'
 
 loop do
   system 'clear'
-    puts "SIMPLE TRANSLATER APP(Supports UTF-8 encoding and a maximum of 500 bytes)\n\n"
+    puts "SIMPLE TRANSLATER APP(Supports UTF-8 encoding and a maximum of 500 bytes)\n\n".colorize(:blue)
 
     select_lang = TTY::Prompt.new
     languages = %w[en pt-BR es fr]
-    src_l = select_lang.select("SELECT YOUR SOURCE LANGUAGE:\n", languages)
-    trg_l = select_lang.select("SELECT THE TARGED LANGUAGE:\n", languages)
+    src_l = select_lang.select("SELECT YOUR SOURCE LANGUAGE:\n".colorize(:blue), languages.colorize(:blue))
+    trg_l = select_lang.select("SELECT THE TARGED LANGUAGE:\n".colorize(:blue), languages.colorize(:blue))
     if src_l == trg_l
-      puts "PLEASE SELECT TWO DISTINCT LANGUAGES"
+      puts "PLEASE SELECT TWO DISTINCT LANGUAGES".colorize(:red)
       next
     end
 
-    puts "\nWRITE YOUR SENTENCE:\n"
+    puts "\nWRITE YOUR SENTENCE:\n".colorize(:blue)
     src_sentence = gets.chomp
     puts "\n"
 
 
-    spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :classic)
+    spinner = TTY::Spinner.new("[:spinner] Loading ...".colorize(:blue), format: :classic)
     spinner.auto_spin
 
     translater_output = Translator.new
     output = translater_output.translater(src_sentence, src_l, trg_l)
 
-    spinner.success("Successful!\n")
+    spinner.success("Successful!\n".colorize(:green))
 
-    puts "YOUR TRANSLATER:\n#{output}\n\n"
+    puts "YOUR TRANSLATER:\n#{output}\n\n".colorize(:green)
 
     save_prompt = TTY::Prompt.new
-    select_s = ['YES', 'NO']
-    q_save = save_prompt.select("SAVE TRANSLATION TO LOG?", select_s)
+    select_s = ['YES'.colorize(:blue), 'NO'.colorize(:blue)]
+    q_save = save_prompt.select("SAVE TRANSLATION TO LOG?".colorize(:blue), select_s)
     if q_save == 'YES'
         s_file = Saver_text.new
         s_file.save_log(src_sentence, output)
     end
 
     loop_prompt = TTY::Prompt.new
-      select = ['YES', 'NO']
-    select_loop = loop_prompt.select("\nDO YOU WANT TO TRY AGAIN", select)
+      select = ['YES'.colorize(:blue), 'NO'.colorize(:blue)]
+    select_loop = loop_prompt.select("\nDO YOU WANT TO TRY AGAIN".colorize(:blue), select)
   break if select_loop == 'NO'
 end
